d1d0f67afd853df3b6711748f1d82fa32b7c30ef
Scott <scott.ty.ma@gmail.com>
2023-10-20 13:09:07 -0500

    Clerk Auth Setup

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="h3-bold">Hello Nextjs!</h2>
-    </main>
-  );
-}
